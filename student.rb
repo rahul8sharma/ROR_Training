8175a75ad250f2cee6d60e9e3f0b47ffc72e4406
Rahul Sharma <rahulsharmadholpur40@gmail.com>
2021-01-23 14:29:43 +0530

    Checked pass of student

--- student.rb
+++ student.rb
@@ -1,5 +1,6 @@
 class Student
   @@students = []
+
   def add(name, roll_number, marks)
     @@students << {
       name: name,
@@ -8,13 +9,19 @@ class Student
     }
   end
 
-  def get_students
+  def all
     @@students
+  end
+
+  def pass?(name)
+    result = @@students.any? {|student| (student[:name] == name && student[:marks] >= 35)}
+    result ? 'Pass' : 'Fail'
   end
 end
 
 student = Student.new
 student.add('Rahul Sharma', 1, 20)
 student.add('Raj Sharma', 2, 50)
-
-puts student.get_students
+puts student.all
+puts student.pass?('Rahul Sharma')
+puts student.pass?('Raj Sharma')

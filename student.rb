43e694a1060703548a032c2c7a088849493f8c99
Rahul Sharma <rahulsharmadholpur40@gmail.com>
2021-01-23 15:21:07 +0530

    Added method to check student grades

--- student.rb
+++ student.rb
@@ -17,6 +17,20 @@ class Student
     result = @@students.any? {|student| (student[:name] == name && student[:marks] >= 35)}
     result ? 'Pass' : 'Fail'
   end
+
+  def get_grade(name)
+    student = @@students.find {|student| student[:name] == name}
+    case student[:marks]
+    when 81..100
+      'A'
+    when 60..80
+      'B'
+    when 40..59
+      'C'
+    else
+      'D'
+    end
+  end
 end
 
 student = Student.new
@@ -25,3 +39,5 @@ student.add('Raj Sharma', 2, 50)
 puts student.all
 puts student.pass?('Rahul Sharma')
 puts student.pass?('Raj Sharma')
+puts student.get_grade('Rahul Sharma')
+puts student.get_grade('Raj Sharma')
